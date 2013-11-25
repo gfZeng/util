0d0fb195d3014552f84680cce85f2a7bb239e60d
Gaofeng Zeng <ndtm.idea@gmail.com>
2013-11-25 15:32:54 +0800

    add Girl object in util module

--- python/util.py
+++ python/util.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
-import threading
+import threading, Queue
 
-class fut(threading.Thread):
+class future(threading.Thread):
     def __init__(self, fn, *args, **kwargs):
         threading.Thread.__init__(self)
         self.fn = lambda: fn(*args, **kwargs)
@@ -20,10 +21,39 @@ class fut(threading.Thread):
 
 def deref(f, timeout_ms=None, timeout_val=None):
     return f.deref(timeout_ms, timeout_val)
+#deref = future.deref
 
 def pmap(f, *seqs):
-    return map(deref, map(lambda args: fut(f, *args), zip(*seqs)))
+    return map(deref, map(lambda args: future(f, *args), zip(*seqs)))
 
+############ p_map is not thread safe ##########
+def p_map(f, seqs, pool_size=30):
+    q = Queue.Queue(pool_size)
+    ret = []
+    argvs = zip(seqs)
+    #argvs = seqs
+    def worker():
+        for argv in argvs:
+            q.put(future(f, *argv))
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
+    class ResultSet(object):
+        def __init__(self, q, capacity):
+            self.q = q
+            self.capacity = capacity
+        def next(self):
+            if self.capacity > 0:
+                self.capacity -= 1
+                return deref(self.q.get())
+            raise Exception("no more elements", "idiot!!!")
+        def has_next(self):
+            return self.capacity > 0
+    return ResultSet(q, len(argvs))
+        
+############################## Girl ################################
+# Girl is a Observable object, implements by use Closure
+####################################################################
 class Girl(object):
     def __init__(self, o):
         self.o = o
@@ -45,6 +75,35 @@ class Girl(object):
     def __call__(self):
         return self.o
 
+'''
+class Girl(object):
+    def __init__(self, v):
+        self.v = v
+        self.wathers = []
+
+    def add_watch(self, k, w):
+        self.watchers.append(lambda r, ov, nv: w(k, r, ov, nv))
+
+    def _notify_watchers(ov, nv):
+        for w in self.watchers:
+            try:
+                w(self, ov, nv)
+            except e:
+                print e
+
+    def reset(self, nv):
+        ov = self.v
+        self.v = nv
+        _notify_watchers(ov, self.v)
+        return nv
+
+    def swap(self, fn, *args, **kwargs):
+        ov = self.v
+        self.v = fn(self.v, *args, **kwargs)
+        _notify_watchers(ov, self.v)
+        return nv
+'''
+
 def add_watch(g, k, w):
     g.add_watch(k, w)
 
@@ -56,3 +115,35 @@ def swap(g, fn, *args, **kwargs):
 
 def  inc(x):
     return x + 1
+
+def dostream(ret, *ls):
+    for l in ls:
+        if hasattr(l, '__call__'):
+            ret = l(ret)
+        elif type(l) in (tuple, list):
+            ret = l[0](ret, *l[1:]) 
+        else:
+            raise Exception("bad arguments", l)
+    return ret
+
+def donestream(ret, *ls):
+    for l in ls:
+        if hasattr(l, '__call__'):
+            ret = l(ret)
+        elif type(l) in (tuple, list):
+            ret = l[0](*(l[1:]+type(l)((ret,)))) 
+        else:
+            raise Exception("bad arguments", l)
+    return ret
+
+def key(item): return item[0]
+get = dict.__getitem__
+
+def val(item): return item[1]
+def nth(l, idx): return l[idx]
+
+def first(l): return l[0]
+
+def second(l): return l[1]
+
+def last(l): return l[-1]
