c0a08ccc8f2b10224e26d70f6f10ca3a70014e81
Gaofeng Zeng <ndtm.idea@gmail.com>
2014-01-04 19:50:14 +0800

    FORMAT: coding

--- c/search-tree.c
+++ c/search-tree.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 #define  LEN 36
 
 struct list {
@@ -22,7 +23,7 @@ make_node()
 {
     py_tree *node = (py_tree *) malloc(sizeof(struct py_tree));
     for (int i = 0; i < LEN; i++)
-	node->next[i] = NULL;
+        node->next[i] = NULL;
     node->ids = NULL;
     return node;
 }
@@ -33,14 +34,14 @@ void
 free_tree(py_tree *tree)
 {
     for (struct list *id = tree->ids; id != NULL;) {
-	struct list *next = id->next;
-	free(id);
-	id = next;
+        struct list *next = id->next;
+        free(id);
+        id = next;
     }
 
     for (int i = 0; i < LEN; i++)
-	if (tree->next[i] != NULL)
-	    free_tree(tree->next[i]);
+        if (tree->next[i] != NULL)
+            free_tree(tree->next[i]);
     free(tree);
 }
 
@@ -48,7 +49,7 @@ int
 get_idx(char c)
 {
     if (c >= '0' && c <= '9')
-	return c - '0';
+        return c - '0';
     return c - 'a' + 10;
 }
 
@@ -65,21 +66,22 @@ void
 push2tree(char *py, char *id)
 {
     if (strlen(id) != 24) {
-	err_ret("bad id");
-	return;
+        err_ret("bad id");
+        return;
     }
 
     py_tree *itree, *node;
     int py_len = strlen(py);
     for (int i = py_len - 1; i >= 0; i--) {
-	itree = tree;
-	for (int j = i; j < py_len; j++) {
-	    node = itree;
-	    itree = itree->next[get_idx(py[j])];
-	    if (itree == NULL)
-		itree = node->next[get_idx(py[j])] = make_node();
-	}
-	itree->ids = add2list(itree->ids, id);
+        itree = tree;
+        for (int j = i; j < py_len; j++) {
+            int idx = get_idx(py[j]);
+            node = itree;
+            itree = itree->next[idx];
+            if (itree == NULL)
+                itree = node->next[idx] = make_node();
+        }
+        itree->ids = add2list(itree->ids, id);
     }
 }
 
@@ -87,7 +89,7 @@ void
 pr_ids(struct list *ids)
 {
     for (struct list *id = ids; id != NULL; id = id->next)
-	printf("%s\t", id->id);
+        printf("%s\t", id->id);
     putchar('\n');
 }
 
@@ -95,32 +97,48 @@ void
 pr_tree(py_tree *tree)
 {
     if (tree == NULL)
-	return;
+        return;
 
     //printf("good\n");
     for (int i = 0; i < LEN; i++) {
-	if (tree->next[i] != NULL) {
-	    printf("%d\n", i);
-	    pr_tree(tree->next[i]);
-	}
+        if (tree->next[i] != NULL) {
+            printf("%d\n", i);
+            pr_tree(tree->next[i]);
+        }
     }
 
     pr_ids(tree->ids);
 }
 
+struct list *
+search(py_tree *tree, const char *s)
+{
+    for (int i = 0, len = strlen(s); i < len; i++) {
+	if (tree == NULL)
+	    return NULL;
+	tree = tree->next[get_idx(s[i])];
+    }
+
+    struct list *ret;
+    return NULL;
+}
+
 int
 main(void)
 {
-    sleep(10);
+    //sleep(10);
     tree = make_node();
 
     push2tree("abc", "abcabcabcabcabcabcabcabc");
     push2tree("aac", "edgabcabcabcabcabcabcabc");
-    pr_tree(tree);
+    push2tree("cccc", "gggggggggggggggggggggggg");
+    printf("%p\n", tree->ids);
+    search(tree, "a");
+    //pr_tree(tree);
 
     //printf("%d\n", get_idx('z'));
 
     free_tree(tree);
-    sleep(10);
+    //sleep(10);
     return 0;
 }
