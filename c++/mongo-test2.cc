15e8704176f22ec6fbdceb177c8d9ec5dfdab1d1
Gaofeng Zeng <ndtm.idea@gmail.com>
2014-01-06 02:05:38 +0800

    very-bad destructed

--- c++/mongo-test2.cc
+++ c++/mongo-test2.cc
@@ -4,12 +4,24 @@
 #include <stdlib.h>
 #include <iostream>
 
+#include <time.h>
 #define ID_LEN         24
 #define N_CHILD        36
+
+#define timing(f) {long t = cur_ms(); f; printf("Elapsed time is: %lds\n", cur_ms() - t);}
 
 using std::vector;
 using std::string;
 using std::auto_ptr;
+
+long
+cur_ms()
+{
+    struct timespec t_spec;
+
+    clock_gettime(CLOCK_REALTIME, &t_spec);
+    return t_spec.tv_sec * 1000 + t_spec.tv_nsec / 1000000;
+}
 
 int
 get_idx(char c)
@@ -21,18 +33,46 @@ get_idx(char c)
 
 class S_List {
 public:
-    S_List         *next;
-    string        *id;
+    S_List	*next;
+    string	*id;
 
     S_List(string *id, S_List *next);
     ~S_List(void);
+
+    S_List *concat(S_List *olist);
 };
+
+void
+pr_ids(S_List *ids)
+{
+    for (S_List *id = ids; id != NULL; id = id->next)
+        printf("%s\t", id->id->c_str());
+        //std::cout <<  *(id->id) << '\t';
+    putchar('\n');
+    //std::cout << std::endl;
+}
+
+S_List *
+S_List::concat(S_List *olist)
+{
+    for (S_List *ilist = this; ilist->next != NULL || (ilist->next = olist); ilist=ilist->next);
+    return this;
+}
 
 S_List::~S_List(void)
 {
     //std::cout << "OH! I'm dead! ======== S_List" << std::endl;
-    if (this->next != NULL)
-        delete this->next;
+    //delete this->id;
+    pr_ids(this);
+    for (; this->next != NULL; this->next = this->next->next) {
+        //free(this->next->id);
+        printf("is that good, ==== %p\n", this->next);
+        std::cout << *this->next->id << std::endl;
+        free(this->next);
+    }
+        printf("is that good 2\n");
+    //if (this->next != NULL)
+    //delete this->next;
 }
 
 S_List::S_List(string *id, S_List *next)
@@ -54,7 +94,31 @@ public:
     void add_Id(string *id);
     Py_Tree *search(const char *s);
     Py_Tree *search(string s);
+    S_List 	*get_all_ids(void);
+    S_List	*get_all_ids(S_List *&ids);
 };
+
+S_List *
+Py_Tree::get_all_ids(S_List *&ids)
+{
+    for (S_List *ilist = this->ids; ilist != NULL; ilist = ilist->next) {
+        ids = new S_List(ilist->id, ids);
+    }
+
+    for (int i = 0; i < N_CHILD; i++) {
+        Py_Tree *pt = this->next[i];
+        pt && pt->get_all_ids(ids);
+    }
+    return ids;
+}
+
+S_List *
+Py_Tree::get_all_ids(void)
+{
+    S_List *l = NULL;
+    this->get_all_ids(l);
+    return l;
+}
 
 Py_Tree *
 Py_Tree::search(const char *s)
@@ -86,6 +150,7 @@ Py_Tree::~Py_Tree(void)
     //std::cout << "OH! I'm dead!" << std::endl;
     delete this->ids;
 
+    std::cout << "OH! I'm dead!" << std::endl;
     for (int i = 0; i < N_CHILD; i++)
         if (this->next[i] != NULL)
             delete this->next[i];
@@ -127,14 +192,6 @@ Py_Tree::fresh(string *py, string *id)
 }
 
 void
-pr_ids(S_List *ids)
-{
-    for (S_List *id = ids; id != NULL; id = id->next)
-        std::cout <<  *(id->id) << '\t';
-    std::cout << std::endl;
-}
-
-void
 pr_tree(Py_Tree *tree)
 {
     if (tree == NULL)
@@ -151,12 +208,29 @@ pr_tree(Py_Tree *tree)
     pr_ids(tree->ids);
 }
 
-int
-main(void)
+void
+foo(void)
 {
+    static int x = 0;
+    printf("%d\n", x++);
+    foo();
+}
+int
+main(int argc, char *argv[])
+{
+    //foo();
+    //S_List *list; 
+    //for (int i = 0; i < 500000; i++) {
+    //    list = new S_List(new string("abc"), list);
+    //}
+    //printf("good news\n");
+    ////sleep(5);
+    //delete list;
+
     Py_Tree *pt = new Py_Tree();
 
-#define host "sz.togic.com"
+    std::cout << cur_ms() << std::endl;
+#define host "localhost"
 #define db string("newhao123")
     mongo::DBClientConnection mc;
     mc.connect(host);
@@ -187,11 +261,15 @@ main(void)
 
     delete is;
 
-    pr_tree(pt->search("A"));
+    //timing(pr_tree(pt->search(argv[1])));
+    for (int i = 1; i < argc; i++) {
+        Py_Tree *ipt = pt->search(argv[i]);
+        timing(if (ipt != NULL) ipt->get_all_ids());
+    }
     //pt->fresh(new string("goodnews"), new string("egfdegfdegfdegfdegfdegfd"));
     //pr_tree(pt);
     //std::cout << *pt->next[24]->next[24]->next[13]->next[23]->next[14]->next[32]->next[28]->ids->id << std::endl;
-    sleep(10);
+    //sleep(10);
     delete pt;
     return 0;
 }
